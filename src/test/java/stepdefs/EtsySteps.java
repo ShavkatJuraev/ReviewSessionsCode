e026124b88bf3b3106be5a58dcc6808c522ff264
shavaju@gmail.com <shavaju@gmail.com>
2018-06-17 13:26:12 -0400

    pom and etsySteps

--- src/test/java/stepdefs/EtsySteps.java
+++ src/test/java/stepdefs/EtsySteps.java
@@ -15,56 +15,34 @@ import junit.framework.Assert;
 import pages.EtsyPage;
 
 public class EtsySteps {
-	
+
 	private WebDriver driver;
 	private EtsyPage etsy;
 	private String keyword;
-	
+
 	@Given("User is on Etsy homepage")
 	public void user_is_on_Etsy_homepage() {
-	    WebDriverManager.chromedriver().setup();
-	    driver = new ChromeDriver();
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	    driver.manage().window().maximize();
-	    driver.get("https://etsy.com");
-	    Assert.assertEquals("Etsy.com | Shop for anything from creative people everywhere", driver.getTitle());
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://etsy.com");
+		Assert.assertEquals("Etsy.com | Shop for anything from creative people everywhere", driver.getTitle());
 	}
 
 	@When("User searches for {string}")
 	public void user_searches_for(String keyword) {
-		this.keyword=keyword;
+		this.keyword = keyword;
 		etsy = new EtsyPage(driver);
 		etsy.searchBox.sendKeys(keyword + Keys.ENTER);
-	   
+		System.out.println("SEARCHING FOR "+ keyword);
+
 	}
 
 	@Then("Search results should be displayed")
 	public void search_results_should_be_displayed() {
 		Assert.assertTrue(driver.getTitle().toLowerCase().startsWith(keyword));
-	    
+
 	}
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
